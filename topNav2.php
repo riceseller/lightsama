750ef89637ad4aa7d536d965acf6176aa2a8b1a2
luokerenz <luokerenz@gmail.com>
2016-06-17 23:40:05 +0800

    search bar bug fix

--- topNav2.php
+++ topNav2.php
@@ -151,7 +151,7 @@
             
             <div id='search'>
                 <form id='searchBox' action="../keyword.php" method="get">
-                    <input type="text" name="search_text" id="search_text" placeholder="Search"/>
+                    <input type="text" name="search" id="search_text" placeholder="Search"/>
                     <input type="button" name="search_button" id="search_button"></a>
                 </form>
             </div>
