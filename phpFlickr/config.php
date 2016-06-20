628ffe40ece3589d67fc7b02315b832027a19f3b
luokerenz <luokerenz@gmail.com>
2016-06-20 20:31:57 +0800

    ....

--- phpFlickr/config.php
+++ phpFlickr/config.php
@@ -4,5 +4,5 @@
 // You can get these from http://www.flickr.com/services/apps/create/apply/
 
 $flickrApiKey = 'af11215c6fb85ece5c24edf3dbf00581';
-$flickrApiSecret = '﻿0c3468606f69b344';
+$flickrApiSecret = '0c3468606f69b344';
 
