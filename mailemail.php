c70918a1f7fdcfcca2c28d821a4290714f2cf828
yichen1992 <cheng.yic@Yichens-MacBook-Pro.local>
2016-06-20 14:35:55 +0800

    .

--- mailemail.php
+++ mailemail.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once '../users/classes/phpmailer/class.phpmailer.php';
-require_once '../users/classes/phpmailer/PHPMailerAutoload.php';
-require_once '../users/classes/phpmailer/class.smtp.php';
+require_once '/users/classes/phpmailer/class.phpmailer.php';
+require_once '/users/classes/phpmailer/PHPMailerAutoload.php';
+require_once '/users/classes/phpmailer/class.smtp.php';
 
 $mail = new PHPMailer;
 
