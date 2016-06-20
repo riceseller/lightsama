18c67ca64ce9565c272f4e79f5281621c924346e
luokerenz <luokerenz@gmail.com>
2016-06-20 17:44:44 +0800

    test3

---
+++ phpFlickr/config.php
@@ -0,0 +1,8 @@
+<?php
+
+// Edit the lines below to add your Flickr API Key and Secret
+// You can get these from http://www.flickr.com/services/apps/create/apply/
+
+$flickrApiKey = 'af11215c6fb85ece5c24edf3dbf00581';
+$flickrApiSecret = '﻿0c3468606f69b344';
+
