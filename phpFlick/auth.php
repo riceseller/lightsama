b8998ac2dda6fd95cecc17f30aa1c5cc4aeb7963
luokerenz <luokerenz@gmail.com>
2016-06-21 01:30:46 +0800

    gg

--- phpFlick/auth.php
+++ phpFlick/auth.php
@@ -34,13 +34,6 @@ if(!empty($_GET['oauth_token']) && !empty($_GET['oauth_verifier'])){
     {
         die("Hmm, something went wrong...\n");
     }
-}else{
-    $oauthToken = @$_GET['oauth_token'];
-    $oauthVerifier = @$_GET['oauth_verifier'];
-    // Looks like we're in the callback
-    $flickr->setOauthData(self::OAUTH_REQUEST_TOKEN, $oauthToken);
-    $flickr->setOauthData(self::OAUTH_VERIFIER, $oauthVerifier);
-    $flickr->setOauthData(self::IS_AUTHENTICATING, false);
 }
 
 $userNsid = $flickr->getOauthData(Flickr::USER_NSID);
