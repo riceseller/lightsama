cd2b03de9a3f59c4cc4689ca88c27136e857e93c
yichen1992 <cheng.yic@Yichens-MacBook-Pro.local>
2016-06-20 14:32:53 +0800

    .

--- mailemail.php
+++ mailemail.php
@@ -13,8 +13,8 @@ $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
 $mail->Username = '110024089a@gmail.com';                 // SMTP username
 $mail->Password = 'GAOsanbaban67890';                           // SMTP password
-$mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 587;                                    // TCP port to connect to
+$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
+$mail->Port = 465;                                    // TCP port to connect to
 
 $mail->setFrom('110024089a@gmail.com', 'Mailer');
 $mail->addAddress('cheng.yic@husky.neu.edu', 'Yichen Cheng');     // Add a recipient
