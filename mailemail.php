0fad520175ed67a7bc3909720e263a071a534f6a
yichen1992 <cheng.yic@Yichens-MacBook-Pro.local>
2016-06-21 18:59:14 +0800

    .

--- mailemail.php
+++ mailemail.php
@@ -5,7 +5,7 @@ require_once 'users/init.php';
 
 $mail = new PHPMailer;
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+$mail->SMTPDebug = 2;                               // Enable verbose debug output
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
