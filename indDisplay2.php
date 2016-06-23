ef3f4308897ae25ed4326f1eba1a3966469eee71
luokerenz <luokerenz@gmail.com>
2016-06-23 16:48:08 +0800

    add two features

--- indDisplay2.php
+++ indDisplay2.php
@@ -18,6 +18,11 @@ else{
 $query = "select c.*, u.url, u.urlSource, su.* from Common c left join Url u on c.p_id=u.id join ScrapeUser su on c.userBelong=su.id where c.p_id=$pid";
 $result=$conn->query($query);
 $row = mysqli_fetch_array($result);
+
+/*$userQuery = "";
+$resultUser = $conn->query($userQuery);
+$rowU = mysqli_fetch_array($resultUser);*/
+
 #page need to delete in future version
 $page = 1;
 
@@ -449,11 +454,13 @@ else{
                         {
                             echo "<p> Need Scrape </p>";
                         }
-                        else
+                        elseif($row['displayName'])
                         {
-                            echo "<p> ".$row[displayName]." </p>";
+                            echo "<p><a href=\"../indUser.php?id=$row[id]\">$row[displayName]</a></p>";
                             //echo "<font size=12> ".$row2[model]."</font>";
-                        }
+                        }else{
+                            echo "<p> null displayName </p>";
+                        }                           
                     ?>
                 </div>      
                 <div class="title">
