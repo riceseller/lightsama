5062303d6a4e0486d68de4f4fee61bd7b9a36c73
yichen1992 <cheng.yic@Yichens-MacBook-Pro.local>
2016-06-24 09:15:19 +0800

    .

--- footer.php
+++ footer.php
@@ -2,7 +2,7 @@
     footer{
         width: 100%;
         height: 250px;
-        justify-content: center;
+        justify-content: space-around;
         align-items: center;
         background: black;
         text-decoration: none;
@@ -10,7 +10,24 @@
         position: relative;
         order: 20;
     }
+    
+    footer .logo{
+        width: 35%;
+        height: 245px;
+        display: block;
+        position: relative;
+        order: 1;
+    }
+    
+    p.logo{
+        font-size: 20px;
+        line-height: 1.4em;
+        color: #ffffff;
+    }    
 </style>
 
 <footer>
+    <div class="logo">
+        <p>PHOTOLIB</p>
+    </div>
 </footer>
