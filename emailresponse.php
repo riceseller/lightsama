394e8f725316374f7287024513d353359da7d870
yichen1992 <cheng.yic@Yichens-MacBook-Pro.local>
2016-06-27 20:57:50 +0800

    .

--- emailresponse.php
+++ emailresponse.php
@@ -1,15 +1,15 @@
 <?php
 require_once 'users/init.php';
-include "supplyment/dbAccess.php";
+//include "supplyment/dbAccess.php";
 $purpose=$_GET["purpose"];
 $name=$_POST["name"];
 $email=$_POST["email"];
 $message=$_POST["message"];
 
-$query = $db->query("SELECT email_login FROM email");
-$results = $conn->query($query);
-$row=$result->fetch_assoc();
-echo $row;
+//$query = $db->query("SELECT email_login FROM email");
+//$results = $conn->query($query);
+//$row=$result->fetch_assoc();
+//echo $row;
 
 if($purpose=="bug")
 {
