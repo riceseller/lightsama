a69b9a1ac4951eb3219b8cc5d40ec9c76e1ccd60
yichen1992 <cheng.yic@Yichens-MacBook-Pro.local>
2016-06-27 20:56:15 +0800

    .

--- emailresponse.php
+++ emailresponse.php
@@ -1,21 +1,21 @@
 <?php
 require_once 'users/init.php';
-//include "supplyment/dbAccess.php";
+include "supplyment/dbAccess.php";
 $purpose=$_GET["purpose"];
 $name=$_POST["name"];
 $email=$_POST["email"];
 $message=$_POST["message"];
 
-//$query = $db->query("SELECT email_login FROM email");
-//$results = $conn->query($query);
-//$row=$result->fetch_assoc();
-//echo $row;
+$query = $db->query("SELECT email_login FROM email");
+$results = $conn->query($query);
+$row=$result->fetch_assoc();
+echo $row;
 
-//if($purpose=="bug")
-//{
-//    $body="someone has a bug to report, here is his find: ".$message." ";
+if($purpose=="bug")
+{
+    $body="someone has a bug to report, here is his find: ".$message." ";
     
-//}
+}
 
 
 echo $purpose;
