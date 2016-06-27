59b34ee0aafc3a972a6b7ef9c1ad9209af3630e1
yichen1992 <cheng.yic@Yichens-MacBook-Pro.local>
2016-06-27 20:43:58 +0800

    .

--- emailresponse.php
+++ emailresponse.php
@@ -1,14 +1,26 @@
 <?php
-
+require_once 'init.php';
 $purpose=$_GET["purpose"];
 $name=$_POST["name"];
 $email=$_POST["email"];
 $message=$_POST["message"];
 
+$query = $db->query("SELECT email_login FROM email");
+$results = $conn->query($query);
+$row=$result->fetch_assoc();
+echo $row;
+
+if($purpose=="bug")
+{
+    $body="someone has a bug to report, here is his find: ".$message." ";
+    
+}
+
+/*
 echo $purpose;
 echo $name;
 echo $email;
 echo $message;
-
+*/
 ?>
 
