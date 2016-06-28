6d67f7edf36f4c56659331973981af5689831c33
yichen1992 <cheng.yic@Yichens-MacBook-Pro.local>
2016-06-28 10:14:42 +0800

    .

--- topNav2.php
+++ topNav2.php
@@ -136,7 +136,7 @@
                 -webkit-justify-content: flex-start;
                 justify-content: flex-start;
                 width: 100%;
-                min-height: 110vh;
+                height: 110vh;
                 font-family: "Proxima Nova", "Helvetica Neue", HelveticaNeue, Helvetica, TeXGyreHeros, FreeSans, "Nimbus Sans L", "Liberation Sans", Arial, sans-serif;
                 -webkit-font-smoothing: antialiased;
             } 
