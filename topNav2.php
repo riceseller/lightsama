caba356bcd5f5f4b2d9f1617771d6db84a2613eb
yichen1992 <cheng.yic@Yichens-MacBook-Pro.local>
2016-06-28 09:25:28 +0800

    .

--- topNav2.php
+++ topNav2.php
@@ -136,7 +136,7 @@
                 -webkit-justify-content: flex-start;
                 justify-content: flex-start;
                 width: 100%;
-                height: 100%;
+                min-height: 100%;
                 font-family: "Proxima Nova", "Helvetica Neue", HelveticaNeue, Helvetica, TeXGyreHeros, FreeSans, "Nimbus Sans L", "Liberation Sans", Arial, sans-serif;
                 -webkit-font-smoothing: antialiased;
                 margin-bottom:150px;/* Height of footer*/
