64c26d8ab806f0d43e022faa6b121499da804387
luokerenz <luokerenz@gmail.com>
2016-06-30 18:09:28 +0800

    ...wat

--- github.php
+++ github.php
@@ -6,4 +6,4 @@ if ( $_POST['payload'] ) {
   shell_exec( 'cd /var/www/html/ && git reset --hard HEAD && git pull https://luokerenx:qingheyikan89@github.com/luokerenz/lightsama 2>&1' );
 }
 
-?>hi
+?>
