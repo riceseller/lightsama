c2ed9a426ac6145c49c62bab8a4180abb715c88e
yichen1992 <yichen1992@users.noreply.github.com>
2016-07-07 09:41:05 +0800

    .

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php 
     include 'topNav2.php';
-    require_once 'supplyment/dbAccess.php';
-    
-    //test code for tracing ip address only starts
-    $ip=$_SERVER['REMOTE_ADDR'];
-    $query_ip="insert into ipaddress(pid, address, times) values(0, '$ip', NOW())";
-    $conn->query($query_ip);
-    //test code for tracing ip address only ends
-    
+       
 ?>
 
 <header>       
