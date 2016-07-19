f4c4bfb4490f630c4f5ac6fc34ad6b3861b96f3f
yichen1992 <yichen1992@users.noreply.github.com>
2016-07-19 18:07:32 +0800

    .

--- mailemail.php
+++ mailemail.php
@@ -22,8 +22,14 @@ $mail->addAddress('hicyc@163.com', 'fuck you');     // Add a recipient
 
 $mail->isHTML(true);                                  // Set email format to HTML
 
-$mail->Subject = 'spam attack';
-$mail->Body    = 'spam attack';
+$mail->Subject = 'revenge';
+
+$mail->Body    = 'you ranked us to the library, to the lab, and to the classroom by our score, you dont know how hurtful it is, therefore i remind you how hurtful it is to me'
+        . '       you dont respect students, and the only thing you care is the score performance. you gotta pay for what you have done. '
+        . '       I used to stay in the classroom, nobody cares the shit of my score, and everyone including the teacher ignores me. They have to pay, and you have to pay. '
+        . '       This is just the beginning. I learned the tech on how to kick your ass. You will get everything you deserve.  '
+        . '       This could b a very good article you use to teach your ass teachers how to respect students';
+
 $mail->AltBody = 'test2';
 $counter=0;
 
