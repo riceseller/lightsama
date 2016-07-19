c9ca47931d3b2cddbee5bb3fb54c20ea04569175
yichen1992 <yichen1992@users.noreply.github.com>
2016-07-19 17:18:27 +0800

    .

--- mailemail.php
+++ mailemail.php
@@ -10,13 +10,13 @@ $mail->SMTPDebug = 2;                               // Enable verbose debug outp
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.live.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'hailoinn@hotmail.com';                 // SMTP username
-$mail->Password = 'ChengandYu';                           // SMTP password
+$mail->Username = '110024089a@gmail.com';                 // SMTP username
+$mail->Password = 'GAOsanbaban67890';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 
-$mail->setFrom('hailoinn@hotmail.com', 'Mailer');
-$mail->addAddress('cheng.yic@husky.neu.edu', 'Yichen Cheng');     // Add a recipient
+$mail->setFrom('110024089a@gmail.com', 'Mailer');
+$mail->addAddress('hicyc@163.com', 'Yichen Cheng');     // Add a recipient
 
 $mail->isHTML(true);                                  // Set email format to HTML
 
