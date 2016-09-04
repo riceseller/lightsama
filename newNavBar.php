293ab238ab058ff71c5e4c0136d2f004024a5a84
yichen1992 <yichen1992@users.noreply.github.com>
2016-09-04 14:28:21 -0400

    .

--- newNavBar.php
+++ newNavBar.php
@@ -5,73 +5,43 @@
 
 <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-
-
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
 
 <style>
-    .navbar-default{
-        background-color: black;
-        border-radius:0;
-        margin:0;
-        padding:0;
-    }
-    .navbar-default .navbar-brand {
-        color: white;
-    }
-    .navbar-default .navbar-brand:hover{
-        color: white;
-    }
-    .navbar-default .navbar-nav>li>a {
-        color: white;
-    }
-    .navbar-default .navbar-nav>li>a:focus, .navbar-default .navbar-nav>li>a:hover {
-        color: white;
-        background-color: transparent;
-    }
     .navbar{
-        margin-bottom: 0px;
+        border-radius: 0;
+    }
+    .pull-xs-left{
+        margin-left:5%;
     }
 </style>
 
-
-
-
-
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">HAILOINN</a>
-    </div>
-    <!-- Collect the nav links, forms, and other content for toggling -->
-    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul class="nav navbar-nav">
-        <li><a href="#">EXPLORE</a></li>
-        <li><a href="#">MAPVIEW</a></li>
-      </ul>
-        
-      <ul class="nav navbar-nav navbar-right">
-        <li><a href="#">LOGIN</a></li>
-        <li><a href="#">SIGNUP</a></li>
-      </ul>
-        
-        <form class="navbar-form navbar-right">
-        <div class="input-group">
-            <input type="text" class="form-control" placeholder="Search for...">
-                <span class="input-group-btn">
-                    <button class="btn btn-default" type="button">Go!</button>
-                </span>
-        </div><!-- /input-group -->
-        </form>
-        
-    </div><!-- /.navbar-collapse -->
-  </div><!-- /.container-fluid -->
+<nav class="navbar navbar-fixed-top navbar-dark bg-inverse">
+    
+  <a class="navbar-brand" href="#">HAILOINN</a>
+  
+  <ul class="nav navbar-nav">
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Mapview<span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Explore</a>
+    </li>
+  </ul>
+  
+  <ul class="nav navbar-nav pull-xs-right">
+      <li class="nav-item active">
+      <a class="nav-link" href="#">Login<span class="sr-only">(current)</span></a>
+    </li>
+    <li class="nav-item active">
+      <a class="nav-link" href="#">Register<span class="sr-only">(current)</span></a>
+    </li>
+  </ul>
+  
+  <form class="form-inline pull-xs-left">
+    <input class="form-control" type="text" placeholder="Search Pic Keywords">
+    <button class="btn btn-outline-success" type="submit">Search</button>
+  </form>
+  
 </nav>
