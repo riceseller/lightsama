c8190164e191340b7ddf3115fbe2a9f45128f248
luokerenz <luokerenz@gmail.com>
2016-09-19 22:21:14 -0400

    added security for edit album meta php

--- users/submitAlbumMeta.php
+++ users/submitAlbumMeta.php
@@ -2,14 +2,15 @@
     require_once 'init.php'; 
     require_once '../supplyment/dbAccess.php';
     require_once '../phpFlick/phpFlickr.php';
-    if(isset($_POST['albumID']) && isset($_POST['Atitle']) && isset($_POST['Adescription']) && isset($_POST['flickr_albumID'])) {
+    if(isset($_POST['albumID']) && isset($_POST['Atitle']) && isset($_POST['Adescription']) && isset($_POST['flickr_albumID']) &&($user->isLoggedIn())) {
+        $useriid = $user->data()->id;
         $albumID = $_POST['albumID'];
         $title = $_POST['Atitle'];
         $description = $_POST['Adescription'];
         $api_key                 = "9c7e15fd3e006075c3647c94ee891bd8";
         $api_secret              = "59cd2bc5e832fe79";
         $f = new phpFlickr($api_key, $api_secret);
-        $query2 = "select lu.authToken,sa.albumID from ScrapeAlbum sa join LinkUser lu on sa.scrapeUserID=lu.scrapeUserID where sa.id=$albumID";
+        $query2 = "select lu.authToken,sa.albumID from ScrapeAlbum sa join LinkUser lu on sa.scrapeUserID=lu.scrapeUserID where sa.id=$albumID and lu.usersID=$useriid";
         $result2=$conn->query($query2);
         $row2 = mysqli_fetch_array($result2);
         $f->setToken($row2[authToken]);
@@ -18,5 +19,5 @@
         $query1 = "update ScrapeAlbum set title = $title, description = $description where id=$albumID";
         $result1=$conn->query($query1);
     }else{
-        echo '<script type="text/javascript">alert("error, not enough parameter");</script>';
+        echo '<script type="text/javascript">alert("error, not enough parameter or not logged in");</script>';
     }
