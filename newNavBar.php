548eb8ea55e237e95b6b1bfbf9dfeb7c56c18b96
yichen1992 <yichen1992@users.noreply.github.com>
2016-10-24 15:19:07 -0400

    .

--- newNavBar.php
+++ newNavBar.php
@@ -178,6 +178,17 @@
             <?php 
                 }    
             ?>  
+                    <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="http://example.com" id="supportedContentDropdown" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+                    <div class="dropdown-menu" aria-labelledby="supportedContentDropdown">
+                        <a class="dropdown-item" href="#">Action</a>
+                        <a class="dropdown-item" href="#">Another action</a>
+                        <a class="dropdown-item" href="#">Something else here</a>
+                    </div>
+                    </li>
+                    
+                    
+                    
     </ul>             
     <form class="form-inline" action="../keyword.php" method="get">
         <?php if(empty($_GET['search'])){$sHolder="Search";}else{$sHolder=$_GET['search'];}?>
