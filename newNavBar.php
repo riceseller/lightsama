2b84a72d56f5b31ccd31c0b819a6ee22557287b6
yichen1992 <yichen1992@users.noreply.github.com>
2017-01-19 09:35:05 -0500

    .

--- newNavBar.php
+++ newNavBar.php
@@ -13,6 +13,7 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="../font-awesome-4.6.3/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="../users/css/new_index.css" />
+<link src="https://ajax.googleapis.com/ajax/libs/angularjs/1.4.8/angular.min.js">
 
 <style>
     .navbar{
